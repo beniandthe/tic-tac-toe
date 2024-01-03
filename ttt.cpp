379551edad94f68e8ca70615f4aa8b504063db9e
beniandthe <125909278+beniandthe@users.noreply.github.com>
2024-01-03 12:15:42 -0800

    Source file and header file included

---
+++ ttt.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include "ttt_f.h"
+
+
+int main() {
+
+    introduction();
+
+    draw_grid();
+    
+    playerturn();
+    
+    board_update();
+    
+
+}
